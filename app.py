334119d9d52d3d3f7b483c37ad3bf034732b22be
joash teng <joashteng123@gmail.com>
2022-05-03 20:22:41 +0800

    1.0

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'pretend secret key for testing') #use
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
+admin_email = 'joash@example.com'
+
 @app.route('/')
 def index():
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
     cur.execute('SELECT name FROM students')
-
     students_list = cur.fetchall()
-
     return render_template('index.html', list = students_list, name = session.get('name')) #checks if name cookie has been set up with session
 
 @app.route('/login', methods=['POST'])
@@ -36,7 +36,11 @@ def login_action():
         pass_hash = students_info[0][2]
         valid = bcrypt.checkpw(password.encode(), pass_hash.encode())
 
-        if valid:
+        if valid and email == admin_email:
+            session['name'] = 'admin'
+            return redirect('/')
+
+        elif valid:
             session['name'] = students_info[0][0]
             session['last login'] = str(datetime.now())
             return redirect('/') #go back to home page if email and password checks passed
@@ -80,6 +84,47 @@ def signup_action():
         session['last login'] = str(datetime.now())
         return redirect('/')
     
+@app.route('/subjects')
+def subjects():
 
+    subjects_list = sql_fetch('SELECT subject_title FROM subjects')
+    print(subjects_list)
+
+    return render_template('subjects.html', name=session.get('name'), subjects = subjects_list) #check login name 
+
+@app.route('/subjects', methods=['POST']) #update subjects - add new or delete existing
+def subjects_update():
+
+    new_subject = request.form.get('add_subject')
+    
+    sql_post('INSERT INTO subjects (subject_title) VALUES (%s)', [new_subject]) #add subject
+
+    del_subject = request.form.get('delete_subject')
+    sql_post('DELETE FROM subjects WHERE subject_title = %s', [del_subject]) #delete subject
+    return redirect('/subjects')
+
+#indivudal subject html pages
+@app.route('/subjects/<subject>')
+def subject(subject):
+
+    subject_id = sql_fetch('SELECT id FROM subjects WHERE subject_title = %s', [subject])
+    group_list = sql_fetch('SELECT group_name FROM groups WHERE subject_id = %s', [subject_id[0][0]])
+
+    return render_template('subjects/subject.html', subject = subject, name = session.get('name'), group_list = group_list)
+
+@app.route('/subject_action', methods=['POST'])
+def groups_update():
+
+    subject = request.form.get('subject')
+    new_group = request.form.get('add_group')
+    delete_group = request.form.get('delete_group')
+    subject_id = sql_fetch('SELECT id FROM subjects WHERE subject_title = %s', [subject])
+    sql_post('INSERT INTO groups (subject_id, group_name) VALUES (%s, %s)', [subject_id[0][0], new_group]) #update database with new group
+    sql_post('DELETE FROM groups WHERE group_name = %s', [delete_group]) #update database with new group
+
+    return redirect('/subjects')
+
+
+    
 if __name__ == '__main__': #we need this for heroku to work
     app.run(debug=True)
--- schema.sql
+++ schema.sql
@@ -63,3 +63,7 @@ CREATE TABLE groups_setup (
 INSERT INTO students (name, email, date_joined, password_hash) VALUES ('Bob Example', 'bob@example.com', '2022-04-10','$2b$12$H4kyXALZeuxc11pF.9I7S.waEmGTdtFJfffiX4K7pOKeXTAbay3/.'); -- password123
 INSERT INTO students (name, email, date_joined, password_hash) VALUES ('Sally Example', 'sally@example.com', '2022-04-10','$2b$12$/oygWGX8k6K8iplJnR4Gh.OdPEYK8LllH6zabMklQaJNIOsV9nEUe'); -- password456
 INSERT INTO students (name, email, date_joined, password_hash) VALUES ('Joash Example', 'joash@example.com', '2022-04-09','$2b$12$UlH7q.ty83TzSoTj1v64zeTUqmmAIjzBcdeztHxuC3daZwbHghNRW'); -- password000
+
+-- prepopulate subjects list
+
+INSERT INTO subjects (subject_title) VALUES ('python');
